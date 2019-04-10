f8f8686cd7096a7bca8940706847a8f3a3f077e0
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2019-04-10 08:53:46 -0400

    update commentService sql

--- services/comment.js
+++ services/comment.js
@@ -11,7 +11,7 @@ CommentService.read = (comment_id) => {
 };
 
 CommentService.update = (author_id, comment_id, content_text) => {
-    return db.none('UPDATE comments SET content_text=${content_text}, updatedAt=NOW() WHERE id=${comment_id}, author_id={author_id}', { author_id, comment_id, content_text });
+    return db.none('UPDATE comments SET content_text=${content_text}, updatedAt=NOW() WHERE id=${comment_id}, author_id={author_id};', { author_id, comment_id, content_text });
 };
 
 CommentService.delete = (comment_id) => {
