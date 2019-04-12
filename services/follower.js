c6a48066276b95f00e767edbb742b8b495301839
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2019-04-12 19:14:05 -0400

    fix route and like table

--- services/follower.js
+++ services/follower.js
@@ -3,7 +3,7 @@ const db = require('./pgpromise');
 const FollowerService = {};
 
 FollowerService.create = (id, followed_id) => {
-    return db.none('INSERT INTO followers (person_being_followed_id, person_following_id) VALUES (person_being_followed_id=${followed_id}, person_following_id=${id})', {id, followed_id});
+    return db.none('INSERT INTO followers (person_following_id, person_being_followed_id) VALUES (person_following_id=${id}, person_being_followed_id=${followed_id})', {id, followed_id});
 };
 
 FollowerService.read = (id) => {
