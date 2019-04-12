dabb6a7e3288b21209b026b95c235222089a0681
AaronTeele <32844775+AaronTeele@users.noreply.github.com>
2019-04-11 20:28:38 -0400

    fix route and like table

--- services/like.js
+++ services/like.js
@@ -3,11 +3,11 @@ const db = require('./pgpromise')
 const LikeService = {};
 
 LikeService.create = (post_id, id) => {
-  return db.none('INSERT INTO likes (post_id, person_liked_id) VALUES (${post_id}, ${id})', {post_id, id})
+  return db.none('INSERT INTO likes (post_id, person_liked_id) VALUES (${post_id}, ${id});', {post_id, id})
 };
 
 LikeService.read = (id) => {
-  return db.one('SELECT * FROM likes WHERE person_liked_id=${id}', {id});
+  return db.one('SELECT * FROM likes WHERE person_liked_id=${id};', {id});
 };
 
 LikeService.delete = (post_id, id) => {
